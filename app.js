39f869d77ea8f86884ee309aedb402ff411340bb
Thuso Kharibe <tkharibe@gmail.com>
2020-08-06 13:56:02 +0200

    Route Handlers Outline
    Created outline for route handlers required

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
 const express  = require('express');
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send("hello world!");
+/* ROUTE HANDLERS */
+
+/* WORD ROUTES */
+
+/**
+ * GET /words
+ * Purpose: Get all words in dictionary
+ */
+app.get('/words', (req,res) => {
+    
 })
+
+/* HISTORY ROUTES */
+
+/**
+ * GET /history
+ * Purpose: Get all sentences in history
+ */
+app.get('/history', (req,res) => {
+    
+})
+
+/**
+ * POST /history
+ * Purpose: Save sentence to history
+ */
+app.post('/history', (req,res) => {
+    
+})
+
+/**
+ * DELETE /history/:id
+ * Purpose: Delete sentence from history
+ */
+app.delete('/history/:id', (req, res) => {
+
+})
+
+
 
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
